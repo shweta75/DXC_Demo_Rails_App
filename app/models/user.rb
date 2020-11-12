1043cc52a9e4a5b888db5f1cb8b59021ec2e66f3
Shweta75 <shwetapathade4@gmail.com>
2020-11-12 11:26:59 +0530

    Changed user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :omniauthable,
          :omniauth_providers => [:twitter, :facebook, :linkedin, :google_oauth2, *(:developer if Rails.env.development?)]
 
+         #It will add user to database for Sign In using social Media
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
